e3b6b846494ece482f03730c10af6aa52fced25a
Amaranto <oscarpersonalsispimx@gmail.com>
2025-07-14 08:50:57 -0700

    arreglado codigo qr

--- tablaComputo/urls.py
+++ tablaComputo/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('principalEqui/',views.lista_equipo,name='principalObjs'),
     path('agregarEqui/',views.crear_equipo,name='agregarEqui'),
     path('modifTabEqui/<int:pk>/editar/',views.editar_equipo,name='editarEqui'),
-    path('detallesEqui/<int:pk>', views.detalles, name='detallesTel'),
+    path('detalles/<int:pk>', views.detalles, name='detalles'),
 ]
--- tablaPrincipal/urls.py
+++ tablaPrincipal/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('principalTel/',views.lista_equipo,name='principalObjs'),
     path('agregarTel/',views.crear_equipo,name='agregarTel'),
     path('modifTab/<int:pk>/editar/',views.editar_equipo,name='editarTel'),
-    path('detallesTel/<int:pk>', views.detalles, name='detallesTel'),
+    path('detalles/<int:pk>', views.detalles, name='detalles'),
 ]
